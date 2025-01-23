9b093a4789f2d5f45eccd1ad5b13dbfcd29686a0
Pedro <pedroarcoverde2@gmail.com>
2025-01-23 13:14:06 -0300

    mudança de soma1 e soma2 para somente soma

--- projeto/utils.py
+++ projeto/utils.py
@@ -4,16 +4,16 @@ def validador(cpf):
         if cpf == '00000000000' or cpf == '11111111111' or cpf == '22222222222' or cpf == '33333333333' or cpf == '44444444444' or cpf == '55555555555' or cpf == '66666666666' or cpf == '77777777777' or cpf == '88888888888' or cpf == '99999999999':
             return False
         else:
-            soma1 = 0
+            soma = 0
             for ncpf in range(9):
-                soma1 += int(cpf[ncpf]) * (10 - ncpf)
-            soma1 = soma1 * 10 % 11
-            if soma1 == int(cpf[9]):
-                soma2 = 0
+                soma += int(cpf[ncpf]) * (10 - ncpf)
+            soma = soma * 10 % 11
+            if soma == int(cpf[9]):
+                soma = 0
                 for ncpf in range(10):
-                    soma2 += int(cpf[ncpf]) * (11 - ncpf)
-                soma2 = soma2 * 10 % 11
-                if soma2 == int(cpf[10]):
+                    soma += int(cpf[ncpf]) * (11 - ncpf)
+                soma = soma * 10 % 11
+                if soma == int(cpf[10]):
                     return True
                 else:
                     return False
